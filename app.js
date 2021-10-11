c312253f4d2f14d7db94d35af04388caffce1f56
matisrago <matiassragowicz@hotmail.com>
2021-10-11 14:33:51 -0300

    Subo los archivos al repositorio

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+
+app.get('/',(req,res)=>{
+    const archivoPrincipal = path.join(__dirname,'/views/home.html')
+    res.sendFile(archivoPrincipal)
+})
+
+app.get('/registro',(req,res)=>{
+    const archivoPrincipal = path.join(__dirname,'/views/register.html')
+    res.sendFile(archivoPrincipal)
+})
+
+app.get('/login',(req,res)=>{
+    const archivoPrincipal = path.join(__dirname,'/views/login.html')
+    res.sendFile(archivoPrincipal)
+})
+
+app.use(express.static('public'));
+app.use(express.urlencoded());
+
+app.listen(3030,()=>{
+    console.log('Funciono el servidor')
+})
+
+app.post('/resultadoRegistro',(req,res)=>{
+    req.body
+   res.redirect('/')
+})
+
+app.post('/resultadoLogin',(req,res)=>{
+    req.body
+   res.redirect('/')
+})
